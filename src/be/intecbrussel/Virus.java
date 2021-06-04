da368a6ccdc10f5b926fbea94da48c0dcd6302b4
Alperyilmaz <alperyilmaz.be@gmail.com>
2021-06-04 19:03:14 +0200

    Initial commit

---
+++ src/be/intecbrussel/Virus.java
@@ -0,0 +1,13 @@
+package be.intecbrussel;
+
+public class Virus {
+
+    public static void main(String[] args) {
+
+        Car car = new Car("Mazda", "MX5", "White");
+
+        car.color = "Red";
+
+        System.out.println(car);
+    }
+}
